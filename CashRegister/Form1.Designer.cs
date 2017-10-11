2c6b2e65731963f70cbabaae2fcbc005ff701a32
GarrSaun <garrsaun253@ed.amdsb.ca>
2017-10-11 13:05:07 -0400

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CashRegister
+{
+    partial class calulatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.burgerBox = new System.Windows.Forms.TextBox();
+            this.friesBox = new System.Windows.Forms.TextBox();
+            this.drinkBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.burgerLabel = new System.Windows.Forms.Label();
+            this.friesLabel = new System.Windows.Forms.Label();
+            this.drinkLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.tenderedBox = new System.Windows.Forms.TextBox();
+            this.tenderdLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // burgerBox
+            // 
+            this.burgerBox.Location = new System.Drawing.Point(96, 43);
+            this.burgerBox.Name = "burgerBox";
+            this.burgerBox.Size = new System.Drawing.Size(94, 20);
+            this.burgerBox.TabIndex = 0;
+            this.burgerBox.Text = "0";
+            this.burgerBox.Click += new System.EventHandler(this.burgerBox_Click);
+            // 
+            // friesBox
+            // 
+            this.friesBox.Location = new System.Drawing.Point(96, 92);
+            this.friesBox.Name = "friesBox";
+            this.friesBox.Size = new System.Drawing.Size(94, 20);
+            this.friesBox.TabIndex = 1;
+            this.friesBox.Text = "0";
+            this.friesBox.Click += new System.EventHandler(this.friesBox_Click);
+            // 
+            // drinkBox
+            // 
+            this.drinkBox.Location = new System.Drawing.Point(96, 136);
+            this.drinkBox.Name = "drinkBox";
+            this.drinkBox.Size = new System.Drawing.Size(94, 20);
+            this.drinkBox.TabIndex = 2;
+            this.drinkBox.Text = "0";
+            this.drinkBox.Click += new System.EventHandler(this.drinkBox_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.BackColor = System.Drawing.Color.White;
+            this.goButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.goButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.goButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(21, 177);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(169, 34);
+            this.goButton.TabIndex = 3;
+            this.goButton.Text = "Calculate Total";
+            this.goButton.UseVisualStyleBackColor = false;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // burgerLabel
+            // 
+            this.burgerLabel.AutoSize = true;
+            this.burgerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.burgerLabel.Location = new System.Drawing.Point(12, 50);
+            this.burgerLabel.Name = "burgerLabel";
+            this.burgerLabel.Size = new System.Drawing.Size(65, 13);
+            this.burgerLabel.TabIndex = 4;
+            this.burgerLabel.Text = "# of Burgers";
+            // 
+            // friesLabel
+            // 
+            this.friesLabel.AutoSize = true;
+            this.friesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.friesLabel.Location = new System.Drawing.Point(26, 99);
+            this.friesLabel.Name = "friesLabel";
+            this.friesLabel.Size = new System.Drawing.Size(51, 13);
+            this.friesLabel.TabIndex = 5;
+            this.friesLabel.Text = "# of Fries";
+            // 
+            // drinkLabel
+            // 
+            this.drinkLabel.AutoSize = true;
+            this.drinkLabel.BackColor = System.Drawing.Color.Transparent;
+            this.drinkLabel.Location = new System.Drawing.Point(18, 143);
+            this.drinkLabel.Name = "drinkLabel";
+            this.drinkLabel.Size = new System.Drawing.Size(59, 13);
+            this.drinkLabel.TabIndex = 6;
+            this.drinkLabel.Text = "# of Drinks";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BackColor = System.Drawing.Color.White;
+            this.totalLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(24, 225);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(166, 60);
+            this.totalLabel.TabIndex = 7;
+            // 
+            // tenderedBox
+            // 
+            this.tenderedBox.Location = new System.Drawing.Point(96, 288);
+            this.tenderedBox.Name = "tenderedBox";
+            this.tenderedBox.Size = new System.Drawing.Size(94, 20);
+            this.tenderedBox.TabIndex = 8;
+            this.tenderedBox.Text = "0";
+            this.tenderedBox.Click += new System.EventHandler(this.tenderedBox_Click);
+            // 
+            // tenderdLabel
+            // 
+            this.tenderdLabel.AutoSize = true;
+            this.tenderdLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tenderdLabel.Location = new System.Drawing.Point(-2, 291);
+            this.tenderdLabel.Name = "tenderdLabel";
+            this.tenderdLabel.Size = new System.Drawing.Size(92, 13);
+            this.tenderdLabel.TabIndex = 9;
+            this.tenderdLabel.Text = "Amount Tendered";
+            // 
+            // changeButton
+            // 
+            this.changeButton.BackColor = System.Drawing.Color.White;
+            this.changeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.changeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.changeButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(29, 314);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(126, 21);
+            this.changeButton.TabIndex = 10;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.changeButton.UseVisualStyleBackColor = false;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.BackColor = System.Drawing.Color.White;
+            this.changeLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(12, 338);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(172, 23);
+            this.changeLabel.TabIndex = 11;
+            this.changeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.BackColor = System.Drawing.Color.White;
+            this.receiptButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.receiptButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.receiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.receiptButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(15, 387);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(169, 34);
+            this.receiptButton.TabIndex = 12;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = false;
+            this.receiptButton.Visible = false;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.White;
+            this.newButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.newButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.newButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newButton.Location = new System.Drawing.Point(318, 2);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(102, 25);
+            this.newButton.TabIndex = 13;
+            this.newButton.Text = "New Order";
+            this.newButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Visible = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // calulatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(488, 450);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderdLabel);
+            this.Controls.Add(this.tenderedBox);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.drinkLabel);
+            this.Controls.Add(this.friesLabel);
+            this.Controls.Add(this.burgerLabel);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.drinkBox);
+            this.Controls.Add(this.friesBox);
+            this.Controls.Add(this.burgerBox);
+            this.Name = "calulatorForm";
+            this.Text = "Cash Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox burgerBox;
+        private System.Windows.Forms.TextBox friesBox;
+        private System.Windows.Forms.TextBox drinkBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label burgerLabel;
+        private System.Windows.Forms.Label friesLabel;
+        private System.Windows.Forms.Label drinkLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox tenderedBox;
+        private System.Windows.Forms.Label tenderdLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Button newButton;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,271 @@
+﻿///Garrett Cash Register Program
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace CashRegister
+{
+    public partial class calulatorForm : Form
+    {
+        //Global Variables
+        //Global Input Variables
+        int burgers;
+        int fries;
+        int drinks;
+        double amountTendered;
+
+        //Global Constants
+        const double BURGER_PRICE = 2.49;
+        const double FRIES_PRICE = 1.89;
+        const double DRINKS_PRICE = 0.99;
+        const double TAX_RATE = 0.13;
+
+        //Global Variables That Need to be Calculated
+        double burgerCost;
+        double friesCost;
+        double drinksCost;
+        double subTotal;
+        double totalTax;
+        double total;
+        double change;
+        int orderNumber;
+
+
+        public calulatorForm()
+        {
+            InitializeComponent();
+        }
+
+        //Remove Text From Textboxes When Clicked
+        private void burgerBox_Click(object sender, EventArgs e)
+        {
+            burgerBox.Text = "";
+        }
+
+        private void friesBox_Click(object sender, EventArgs e)
+        {
+            friesBox.Text = "";
+        }
+
+        private void drinkBox_Click(object sender, EventArgs e)
+        {
+            drinkBox.Text = "";
+        }
+
+        private void tenderedBox_Click(object sender, EventArgs e)
+        {
+            tenderedBox.Text = "";
+        }
+
+        //Calculate Cost with Tax
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                burgers = Convert.ToInt16(burgerBox.Text);
+                fries = Convert.ToInt16(friesBox.Text);
+                drinks = Convert.ToInt16(drinkBox.Text);
+
+                burgerCost = burgers * BURGER_PRICE;
+                friesCost = fries * FRIES_PRICE;
+                drinksCost = drinks * DRINKS_PRICE;
+
+                subTotal = burgerCost + friesCost
+                    + drinksCost;
+                totalTax = (burgerCost + friesCost
+                    + drinksCost) * TAX_RATE;
+                total = totalTax + subTotal;
+
+                totalLabel.Text = "Sub Total: " + subTotal.ToString("$0.00") + "\n" +
+                    "Tax: " + totalTax.ToString("$0.00") + "\n" +
+                    "Total: " + total.ToString("$0.00");
+            }
+
+            catch
+            {
+                totalLabel.Text = "Something went wrong. Try again!";
+                return;
+            }
+
+        }
+
+        //Calculate Change
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                amountTendered = Convert.ToDouble(tenderedBox.Text);
+
+                change = amountTendered - total;
+
+                changeLabel.Text = "Change: " + change.ToString("$0.00");
+
+                receiptButton.Visible = true;
+            }
+
+            catch
+            {
+                changeLabel.Text = "Something went wrong.";
+                return;    
+            }
+        }
+
+        //Receipt
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            //Sound
+            SoundPlayer printPlayer = new SoundPlayer(Properties.Resources.print);
+            printPlayer.Play();
+
+            //Set up graphics
+            Graphics fg = this.CreateGraphics();
+            Pen receiptPen = new Pen(Color.Black, 2);
+            SolidBrush receiptBrush = new SolidBrush(Color.White);
+            Font printFont = new Font("Segoe UI", 8, FontStyle.Bold);
+            SolidBrush printBrush = new SolidBrush(Color.Black);
+
+            //Draw receipt
+            fg.Clear(Color.LightGray);
+
+            orderNumber = orderNumber + 1;
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 25);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 25);
+
+            Thread.Sleep(250); //1
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 50);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 50);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+
+            Thread.Sleep(250); //2
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 100);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 100);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+
+            Thread.Sleep(250); //3
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 150);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 150);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+
+            Thread.Sleep(250); //4
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 200);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 200);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+            fg.DrawString("Drinks x" + drinks + " @" +
+                DRINKS_PRICE.ToString("$0.00"), printFont, printBrush, 275, 210);
+
+            Thread.Sleep(250); //5
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 250);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 250);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+            fg.DrawString("Drinks x" + drinks + " @" +
+                DRINKS_PRICE.ToString("$0.00"), printFont, printBrush, 275, 210);
+            fg.DrawString("Subtotal =" + subTotal.ToString("$0.00")
+                , printFont, printBrush, 275, 260);
+
+            Thread.Sleep(250); //6
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 300);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 300);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+            fg.DrawString("Drinks x" + drinks + " @" +
+                DRINKS_PRICE.ToString("$0.00"), printFont, printBrush, 275, 210);
+            fg.DrawString("Subtotal =" + subTotal.ToString("$0.00")
+                , printFont, printBrush, 275, 260);
+            fg.DrawString("Tax =" + totalTax.ToString("$0.00")
+                , printFont, printBrush, 275, 310);
+
+            Thread.Sleep(250); //7
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 350);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 350);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+            fg.DrawString("Drinks x" + drinks + " @" +
+                DRINKS_PRICE.ToString("$0.00"), printFont, printBrush, 275, 210);
+            fg.DrawString("Subtotal =" + subTotal.ToString("$0.00")
+                , printFont, printBrush, 275, 260);
+            fg.DrawString("Tax =" + totalTax.ToString("$0.00")
+                , printFont, printBrush, 275, 310);
+            fg.DrawString("Total =" + total.ToString("$0.00")
+                , printFont, printBrush, 275, 360);
+
+            Thread.Sleep(250); //8
+
+            fg.DrawRectangle(receiptPen, 250, 30, 225, 400);
+            fg.FillRectangle(receiptBrush, 250, 30, 225, 400);
+            fg.DrawString("Order Number " + orderNumber, printFont, printBrush, 275, 70);
+            fg.DrawString("Hamburgers x" + burgers + " @" +
+                BURGER_PRICE.ToString("$0.00"), printFont, printBrush, 275, 110);
+            fg.DrawString("Fries x" + fries + " @" +
+                FRIES_PRICE.ToString("$0.00"), printFont, printBrush, 275, 160);
+            fg.DrawString("Drinks x" + drinks + " @" +
+                DRINKS_PRICE.ToString("$0.00"), printFont, printBrush, 275, 210);
+            fg.DrawString("Subtotal =" + subTotal.ToString("$0.00")
+                , printFont, printBrush, 275, 260);
+            fg.DrawString("Tax =" + totalTax.ToString("$0.00")
+                , printFont, printBrush, 275, 310);
+            fg.DrawString("Total =" + total.ToString("$0.00")
+                , printFont, printBrush, 275, 360);
+            fg.DrawString("Amount Tendered =" + amountTendered.ToString("$0.00") + "\n" +
+                "Change Due = " + change.ToString("$0.00"), printFont, printBrush, 275, 400);
+
+            newButton.Visible = true;
+            receiptButton.Visible = false;
+        }
+
+        //New order
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            burgers = 0;
+            fries = 0;
+            drinks = 0;
+            amountTendered = 0;
+
+            burgerBox.Text = "0";
+            friesBox.Text = "0";
+            drinkBox.Text = "0";
+            tenderedBox.Text = "0";
+            totalLabel.Text = "";
+            changeLabel.Text = "";
+
+            newButton.Visible = false;
+        }
+    }
+}
+
